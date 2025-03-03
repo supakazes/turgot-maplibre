1e2b1874fd9e67b67c334ee042b1420e632d63fd
florianboudot <florian.boudot@gmail.com>
2025-03-03 12:24:15 +0100

    feat(app): se model rotation from list

--- src/models-list.ts
+++ src/models-list.ts
@@ -1,15 +1,18 @@
 export interface ModelDefinition {
   path: string;
   origin: { lng: number; lat: number };
+  rotation: number;
 }
 
 export const modelsList: ModelDefinition[] = [
   {
     path: "/src/models/barriere_saint-martin_or_rotonde_de_stalingrad/scene.gltf",
     origin: { lng: 2.3695243103564962, lat: 48.88343284527224 },
+    rotation: 1,
   },
   {
     path: "/src/models/matilda/scene.gltf",
     origin: { lng: 2.370598777169562, lat: 48.885831619533406 },
+    rotation: 0.5,
   },
 ];
