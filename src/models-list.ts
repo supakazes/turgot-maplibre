e274f4c406f10edfd71fb11423a939ce0ce0e1f1
florianboudot <florian.boudot@gmail.com>
2025-03-10 16:20:14 +0100

    feat(app): hover model change scale

--- src/models-list.ts
+++ src/models-list.ts
@@ -1,4 +1,5 @@
 export interface ModelDefinition {
+  id: string;
   path: string;
   origin: { lng: number; lat: number };
   rotation: number;
@@ -6,16 +7,19 @@ export interface ModelDefinition {
 
 export const modelsList: ModelDefinition[] = [
   {
+    id: "house-1",
     path: "/src/models/house/house.glb",
     origin: { lng: 2.369540152077093, lat: 48.8834462031036 },
     rotation: 0.9,
   },
   {
+    id: "house-2",
     path: "/src/models/house/house.glb",
     origin: { lng: 2.3686339204380147, lat: 48.88446609167394 },
     rotation: 0.5,
   },
   {
+    id: "house-3",
     path: "/src/models/house/house.glb",
     origin: { lng: 2.371983100211537, lat: 48.88527557732877 },
     rotation: 0.5,
