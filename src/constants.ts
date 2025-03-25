0fbc7a707520dc4eadd345e0b7701da1a17ab58d
florianboudot <florian.boudot@gmail.com>
2025-03-25 14:43:38 +0100

    feat(app): add plan de turgot as image layer

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,2 @@
-import maplibregl from "maplibre-gl";
-
 export const MAPTILER_KEY = "Lr1q5wml4SB6TlSXbQrJ";
-export const SCENE_ORIGIN = new maplibregl.LngLat(2.370572296723708, 48.884197153165246);
+export const SCENE_ORIGIN = { lng: 2.360444120820034, lat: 48.851535541639635 };
