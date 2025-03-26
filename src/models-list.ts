5fa198cdc5c7eff104ce4bed089f6a7234eb52a7
florianboudot <florian.boudot@gmail.com>
2025-03-26 15:03:35 +0100

    refactor(build): relative paths test

--- src/models-list.ts
+++ src/models-list.ts
@@ -8,19 +8,19 @@ export interface ModelDefinition {
 export const modelsList: ModelDefinition[] = [
   {
     id: "house-1",
-    path: "src/models/house/house.glb",
+    path: "./src/models/house/house.glb",
     origin: { lng: 2.359989916741256, lat: 48.851052162943716 },
     rotation: 0.9,
   },
   {
     id: "house-2",
-    path: "src/models/house/house.glb",
+    path: "./src/models/house/house.glb",
     origin: { lng: 2.362784417090552, lat: 48.85142690508661 },
     rotation: 0.5,
   },
   {
     id: "house-3",
-    path: "src/models/house/house.glb",
+    path: "./src/models/house/house.glb",
     origin: { lng: 2.360932797829946, lat: 48.85244828197375 },
     rotation: 0.5,
   },
