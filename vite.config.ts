79ad8698a10e3f3676eaebd8a1df873d4016fa0a
florianboudot <florian.boudot@gmail.com>
2025-03-26 15:03:35 +0100

    refactor(build): relative paths test

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  base: "./",
+  base: "",
   resolve: {
     alias: {
       src: path.resolve(__dirname, "src"),
