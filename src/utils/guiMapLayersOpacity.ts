9fac4f5b1135fec065bd693e56aec61768056818
florianboudot <florian.boudot@gmail.com>
2025-03-31 17:30:03 +0200

    feat(app): add all 20 image sheets from Turgot Map

--- src/utils/guiMapLayersOpacity.ts
+++ src/utils/guiMapLayersOpacity.ts
@@ -1,21 +1,21 @@
 import { sourceMapSheets } from "src/mapRasterSources";
 
-function guiMapLayersOpacity(gui: dat.GUI, map: maplibregl.Map) {
-  const layer = {
-    opacity: 0,
-  };
+export const SHEET_LAYER = {
+  opacity: 0.5,
+};
 
+function guiMapLayersOpacity(gui: dat.GUI, map: maplibregl.Map) {
   const opacityFolder = gui.addFolder("Raster Layers Opacity");
 
   opacityFolder
-    .add(layer, "opacity", 0, 1, 0.01)
+    .add(SHEET_LAYER, "opacity", 0, 1, 0.01)
     .name("Map Overview")
     .onChange((value) => {
       map.setPaintProperty("map-overview", "raster-opacity", value);
     });
 
   opacityFolder
-    .add(layer, "opacity", 0, 1, 0.01)
+    .add(SHEET_LAYER, "opacity", 0, 1, 0.01)
     .name("Map Sheets")
     .onChange((value) => {
       sourceMapSheets.forEach((source) => {
